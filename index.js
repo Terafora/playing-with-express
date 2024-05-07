6c4bd6537629e34b775a9eabc9ad88e609d83102
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-05-07 10:19:54 +0100

    Setup res.send

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(() => {
+app.use((req, res) => {
     console.log("A new request received at " + Date.now());
+    res.send("Hello, we got your request! This is the response!");
 })
 
 app.listen(port, () => {
